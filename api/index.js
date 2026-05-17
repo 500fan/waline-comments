a5952c5ddd7dc97fd750c5afec45bd19a8071d4a
500fan <392637221@eqq.com>
2026-05-18 05:35:27 +0800

    fix: use @waline/server + express

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
-const { Waline } = require('@waline/vercel');
+const express = require('express');
+const { Waline } = require('@waline/server');
 
-module.exports = Waline({
+const app = express();
+
+app.use(Waline({
   database: 'mongodb',
   mongoDB: process.env.MONGO_URI,
+}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Waline server running on port ${port}`);
 });
