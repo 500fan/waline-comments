f4a311b97aff802d8100c5906072cbe223122bbd
500fan <392637221@eqq.com>
2026-05-18 17:13:20 +0800

    fix: remove GITHUB_PATH patch that overrides env var

--- start.js
+++ start.js
@@ -1,18 +1,2 @@
-// 修补 adapter.js 确保 GITHUB_PATH 不是 undefined
-const fs = require('fs');
-const adapterPath = 'node_modules/@waline/vercel/src/config/adapter.js';
-
-try {
-  let code = fs.readFileSync(adapterPath, 'utf8');
-  // 确保 GITHUB_PATH 有默认值
-  code = code.replace(
-    'GITHUB_PATH } = process.env;',
-    'GITHUB_PATH = \'\', } = process.env;'
-  );
-  fs.writeFileSync(adapterPath, code, 'utf8');
-  console.log('Patched adapter.js: GITHUB_PATH default set');
-} catch(e) {
-  console.log('Patch skipped:', e.message);
-}
 
 require('@waline/vercel/vanilla.js');
